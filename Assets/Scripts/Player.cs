4244990dc6e493541cb1e82afe2d1d27a690e7e3
ixi150 <ixi150@gmail.com>
2018-11-30 21:16:11 +0100

    created test code

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Player:MonoBehaviour
+    {
+        public Keyboard Keyboard;
+        public Mouse mouse;
+        public PlayerInput input;
+        public ManagerOfSingletos manager;
+    }
+}
