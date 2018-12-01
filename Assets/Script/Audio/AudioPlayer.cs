2b4c6246893fe41c6562066412a06e6ef8675234
ixi150 <ixi150@gmail.com>
2018-12-01 22:06:15 +0100

    Upload major part of audio and music

---
+++ Assets/Script/Audio/AudioPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xunity.ScriptableEvents;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+	[SerializeField] bool playOnEnable;
+	[SerializeField] AudioEvent audioEvent;
+
+	AudioSource audioSource;
+	
+	public void Play()
+	{
+		audioEvent.Play(audioSource);
+	}
+
+	void Awake()
+	{
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void OnEnable()
+	{
+		if (playOnEnable)
+			Play();
+	}
+
+	void OnDisable()
+	{
+		audioSource.Stop();
+	}
+}
