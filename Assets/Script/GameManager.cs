01d5ff7a3489b853f198734bf81efd855c907b18
WhiteWolfIron <wolfmar.ww@gmail.com>
2018-12-01 06:49:49 +0100

    Witch points

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GamepadInput;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public int[] playerPoints = new int[4];
+
+    public void AddPoints(GamePad.Index player, int points)
+    {
+        playerPoints[(int)player] += points;
+    }
+
+    private void OnGUI()
+    {
+        for (int i = 0; i < playerPoints.Length; i++)
+        {
+            GUI.Label(new Rect(50, 25 + i * 25, 200, 25), "Player " + (i + 1) + ": " + playerPoints[i]);
+        }
+    }
+}
---
+++ Assets/Script/Witch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Witch : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        var player = other.GetComponentInParent<Player>();
+        if (player)
+        {
+            GameManager.Instance.AddPoints(player.input.player, player.GetAllEaten().Length);
+        }
+    }
+
+}
