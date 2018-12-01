ace93dce93df26144dfc15f442b2ba9a6e6619bc
Mikołaj Dereszewski <mikolaj.dereszewski@gmail.com>
2018-12-01 02:56:39 +0100

    Created collectible scripting base

---
+++ Assets/Gameplay/Scripts/Items/Collectible.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    public CollectibleType CollectibleType { get { return _collectibleType; } }
+
+    [SerializeField]
+    private CollectibleType _collectibleType;
+}
+
+public enum CollectibleType
+{
+    Mooshroom,
+    Egg,
+    Mandragora
+}
---
+++ Assets/Gameplay/Scripts/Items/CollectibleManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectibleManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static CollectibleManager instance;
+    public static CollectibleManager Instance
+    {
+        get
+        {
+            if(instance == null)
+                instance = FindObjectOfType<CollectibleManager>();
+            return instance;
+        }
+    }
+
+    #endregion
+
+
+
+    #region Properties
+
+    [SerializeField]
+    private List<Collectible> _collectiblePool;
+
+    #endregion
+
+
+
+    #region MonoBehaviour
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        instance = null;
+    }
+
+    #endregion
+
+
+
+    #region Controller
+
+    private Collectible CreateCollectible(Collectible collectible, Vector3 position)
+    {
+        Collectible obj = Instantiate(collectible, position, Quaternion.identity);
+        return obj;
+    }
+
+    #endregion
+}
