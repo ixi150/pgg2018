86de0914b88a7f1c0dc10c6c46346a5178c46dc7
ixi150 <ixi150@gmail.com>
2018-12-02 13:57:47 +0100

    dragon animations

---
+++ Assets/EventRaiser.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Xunity.ScriptableEvents;
+
+public class EventRaiser : MonoBehaviour
+{
+    [SerializeField] GameEvent[] enabledEvents, disabledEvents;
+
+    void OnEnable()
+    {
+        foreach (var gameEvent in enabledEvents)
+        {
+            gameEvent.Raise();
+        }
+    }
+
+    void OnDisable()
+    {
+        foreach (var gameEvent in disabledEvents)
+        {
+            gameEvent.Raise();
+        }
+    }
+}
